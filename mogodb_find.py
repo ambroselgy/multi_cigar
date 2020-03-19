a62bf2ee67852910120ec429d954964b34d06b7b
diya <13610586@qq.com>
2020-03-19 13:51:34 +0800

    by mongodb

---
+++ mogodb_find.py
@@ -0,0 +1,12 @@
+#_*_ coding : UTF-8 _*_
+#开发人员：Diya
+#开发时间：2020/3/19 13:51
+#文件名称: mogodb_find.PY
+#开发工具：PyCharm
+#$lt和<，$lte和<=，$gt和>，gte和>=，ne和!=   $in和$nin  $not 和 $and $or
+
+#{"$and": [{"cigar_name": {"$regex":"Cohiba"}}, {"stock": {"$regex": "available"}}]}
+#{"stock":{$not: {"$regex":"sold",'$options':'i'}}}
+#{"cigar_name":{"$regex":"cohiba",'$options':'i'}}
+#{$and:[{"cigar_name":{"$regex":"cohiba",'$options':'i'}},{"stock":{$not:{"$regex":"sold",'$options':'i'}}}]}
+#{$and:[{"cigar_name":{"$regex":"Cohiba",'$options':'i'}},{"stock":{$not:{"$regex":"sold",'$options':'i'}}}]}
